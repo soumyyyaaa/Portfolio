748d481b6221250aa7abdd5ef0eefd829130cb4a
soumyyyaaa <soumya.agarwal1103@gmail.com>
2022-12-18 04:52:33 +0530

    Footer Added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import Home from "./Page/Home";
 import Contactme from "./Page/Contactme";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbars from "./Components/Navbar/Navbars"
-import Project from "./Page/Project"
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbars from "./Components/Navbar/Navbars";
+import Project from "./Page/Project";
+import Footer from "./Components/Navbar/Footer";
 
 function App() {
   return (
     <div className="App">
-    <Navbars />
-    <Home/>
-    <Project />
-    <Contactme/>
+      <Navbars />
+      <Home />
+      <Project />
+      <Contactme />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Navbar/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./footer.css";
+
+function Footer() {
+  return (
+    <>
+      <div className="footer">
+      <div>
+        <a
+          href="https://github.com/soumyyyaaa"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-icon git"
+        >
+          <i className="fa fa-brands fa-github fa-lg"></i>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/soumya-agrawal-7011661ba/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-icon linked"
+        >
+          <i className="fa fa-brands fa-linkedin fa-lg"></i>
+        </a>
+        <a
+          href="https://www.instagram.com/soumyyyaaa/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-icon insta"
+        >
+          <i className="fa fa-brands fa-instagram fa-lg"></i>
+        </a>
+        </div>
+        <div className="menu">
+            <a href="/home" className="footer-menu">Home</a>
+            <a href="/projects" className="footer-menu">Projects</a>
+            <a href="#contact-me" className="footer-menu">Contact Me</a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
